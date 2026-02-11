57d22c2742ea495c86528c1cfd446c55fed57fba
SergLeft <sleftheriotis@yahoo.com>
2026-02-11 02:25:27 +0100

    Update SparseArray.scala

--- src/main/scala/SparseArray.scala
+++ src/main/scala/SparseArray.scala
@@ -15,18 +15,34 @@ class SparseArray[A](val size: Int, val default: A) {
   // bildet die Arrayposition auf den jeweiligen Wert ab
   private val entries = mutable.HashMap[Int, A]()
 
+  /** Helper: checks if position is valid. */
+  private def checkBounds(position: Int): Unit =
+    if (position < 0 || position >= size)
+      throw IndexOutOfBoundsException(s"Position $position is out of bounds for size $size")
+
   /** Gibt zurück, welches Element an der übergebenen Position gespeichert ist.
    *
    * @throws IndexOutOfBoundsException wenn die Position im Array nicht existiert
    */
-  def load(position: Int): A = ???
+  def load(position: Int): A = {
+    checkBounds(position)
+    entries.getOrElse(position, default)
+  }
 
   /** Schreibt den übergebenen Wert an die übergebene Position im Array.
    *
    * @throws IndexOutOfBoundsException wenn die Position im Array nicht existiert
    */
-  def update(position: Int, value: A): Unit = ???
+  def update(position: Int, value: A): Unit = {
+    checkBounds(position)
+    if (value == default)
+      entries.remove(position)      // No storin default value!
+    else
+      entries.update(position, value)
+  }
 
   /** Gibt eine Sequenz mit allen Einträgen dieses Arrays zurück, inklusive der nicht explizit gespeicherten Standardwerte. */
-  def asDense: Seq[A] = ???
+  def asDense: Seq[A] = {
+    (0 until size).map(i => entries.getOrElse(i, default))
+  }
 }
